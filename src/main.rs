269a75a50b046c7071be22ce69450f31d9b470e6
Lollo <41307169+etscheelk@users.noreply.github.com>
2024-10-07 13:22:45 -0500

    Added chess structs and enums

--- src/main.rs
+++ src/main.rs
@@ -28,11 +28,65 @@ impl MainState
             input_state: InputState {  },
             screen,
             assets,
-            player: Player::default()
+            player: Player::default().feet_offset(Vec2::new(0.0, 10.0))
         };
 
         Ok(s)
     }
+}
+
+#[derive(Default, Debug, Clone)]
+struct Cell<'a>
+{
+    piece: Option<&'a Piece>,
+    index: u16,
+    pos: Vec2,
+    selected: bool
+}
+
+
+#[derive(Default, Debug, Clone)]
+struct Piece
+{
+    piece_type: PieceType,
+    piece_color: PieceColor
+}
+
+impl Piece
+{
+    fn move_piece(&mut self)
+    {
+
+    }
+}
+
+#[derive(Default, Debug, Clone, Copy)]
+#[non_exhaustive]
+enum PieceColor
+{
+    #[default]
+    Uncolored,
+    Light,
+    Dark
+}
+
+#[derive(Default, Debug, Clone, Copy)]
+#[non_exhaustive]
+enum PieceType
+{
+    #[default]
+    Pawn,
+    Rook, 
+    Bishop,
+    Knight,
+    Queen,
+    King,
+}
+
+enum CellColor
+{
+    Light,
+    Dark
 }
 
 struct InputState
@@ -40,11 +94,36 @@ struct InputState
 
 }
 
-#[derive(Default)]
+#[derive(Default, Debug, Clone, derive_setters::Setters)]
 struct Player
 {
     pos: Vec2,
     vel: Vec2,
+    feet_offset: Vec2
+}
+
+impl ggez::event::EventHandler for Player
+{
+    fn update(&mut self, _ctx: &mut ggez::Context) -> Result<(), ggez::GameError> {
+        self.vel.y += 4.9 / 60.0;
+        self.pos += self.vel / 60.0;
+
+        if (self.pos + self.feet_offset).y > 400.0
+        {
+            self.pos.y = 400.0 - self.feet_offset.y;
+        }
+
+        if self.pos.x > 400.0
+        {
+            self.pos.x = 0.0;
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self, _ctx: &mut ggez::Context) -> Result<(), ggez::GameError> {
+        todo!()
+    }
 }
 
 struct Assets
@@ -66,20 +145,19 @@ impl ggez::event::EventHandler for MainState
 {
     fn update(&mut self, context: &mut ggez::Context) -> ggez::GameResult {
         
+        
+
         while context.time.check_update_time(60)
         {
-            self.player.pos.y = 400.0;
-            self.player.pos.x += 1.0;
-            if self.player.pos.x > 800.0
-            {
-                self.player.pos.x = 0.0;
-            }
+            self.player.update(context)?;
         }
 
         Ok(())
     }
 
     fn draw(&mut self, context: &mut ggez::Context) -> ggez::GameResult {
+        // self.player.draw(context)?;
+
         // Our drawing is quite simple.
         // Just clear the screen...
         use ggez::graphics::{self, Color};
@@ -110,6 +188,36 @@ fn main() -> ggez::GameResult
         std::path::PathBuf::from("./resources")
     };
 
+    // let piece_order = 
+    // {
+    //     use PieceType::*;
+    //     vec![Rook,  Bishop, Knight, King,   Queen,  Knight, Bishop, Rook,
+    //          Pawn,  Pawn,   Pawn,   Pawn,   Pawn,   Pawn,   Pawn,   Pawn]
+    // };
+
+    // let mut board: [Cell; 64] = array_macro::array!(Cell::default(); 64);
+
+    // let mut pieces: Vec<Piece> = 
+    //     piece_order
+    //     .iter()
+    //     // .cloned()
+    //     .map(
+    //     |&piece_type|
+    //     {
+    //         Piece { piece_type, ..Default::default() }
+    //     })
+    //     .collect();
+
+    // pieces
+    // .iter_mut()
+    // .zip(board.iter_mut())
+    // .for_each(
+    // |(p, c)|
+    // {
+    //     c.piece = Some(p);
+    // });
+
+    // println!("board: {:#?}", board);
 
     // I hate file-global `use` statements
     // I prefer to aboslutely know where shit is coming from
@@ -117,7 +225,7 @@ fn main() -> ggez::GameResult
     let cb = 
         ContextBuilder::new("chess_thing", "Ethan Scheelk")
         .window_setup(conf::WindowSetup::default().title("Chess thing?"))
-        .window_mode(conf::WindowMode::default().dimensions(800.0, 800.0))
+        .window_mode(conf::WindowMode::default().dimensions(400.0, 400.0))
         .add_resource_path(resource_dir);
 
     let (mut context, event_loop) = cb.build()?;
