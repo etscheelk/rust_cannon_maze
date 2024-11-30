a00bac32d99d4859e74c9e49a19fb59dbae98de2
Lollo <41307169+etscheelk@users.noreply.github.com>
2024-11-29 18:21:54 -0600

    Cannon rotation now experiences accel and decel by arrow keys

--- src/game_object/cannon.rs
+++ src/game_object/cannon.rs
@@ -32,8 +32,8 @@ impl Cannon
 {
     const VELOCITY : f32 = 50.0;
     const MAX_ROT_PER_SEC: f32 = 6.28;
-    const ROT_ACC: f32 = 1.0;
-    const ROT_DE_ACC: f32 = 0.25;
+    const ROT_ACC: f32 = 0.5;
+    const ROT_DE_ACC: f32 = 1.5;
 
     fn new(facing: Vec2, position: Vec2) -> Self
     {
@@ -51,10 +51,48 @@ impl crate::FixedUpdate<Cannon> for crate::MainState
     fn fixed_update(&mut self, context: &mut ggez::Context) -> ggez::GameResult {
         let ref mut cannon = self.cannon;
 
-        let delta_rot: f32;
+        // let delta_rot: f32;
 
         // delta_rot = Cannon::ROT_ACC * context.time.delta().as_secs_f32();
-        delta_rot = Cannon::ROT_ACC / 60.0;
+        // delta_rot = Cannon::ROT_ACC / 60.0;
+
+        // if self.input_state.cannon_rotate.is_some()
+
+        // println!("{:?}, {}, {:?}", self.input_state.cannon_rotate, cannon.rot_vel, cannon.facing);
+        match self.input_state.cannon_rotate
+        {
+            Some(d) =>
+            {
+                use RotateDir::*;
+
+                let mut new_rot_vel: f32;
+                new_rot_vel = cannon.rot_vel + Cannon::ROT_ACC / 60.0 * if d == Left { 1.0 } else { -1.0 };
+                new_rot_vel = new_rot_vel.clamp(-Cannon::MAX_ROT_PER_SEC, Cannon::MAX_ROT_PER_SEC);
+
+                cannon.rot_vel = new_rot_vel;
+                cannon.facing = cannon.facing.rotate_by(cannon.rot_vel / 60.0);
+            },
+            // button not held at this time. 
+            // Apply damping force in the opposite direction
+            None =>
+            {
+                let mut new_rot_vel: f32;
+                if cannon.rot_vel > 0.0
+                {
+                    new_rot_vel = cannon.rot_vel - Cannon::ROT_DE_ACC / 60.0;
+                    new_rot_vel = new_rot_vel.max(0.0);
+
+                }
+                else
+                {
+                    new_rot_vel = cannon.rot_vel + Cannon::ROT_DE_ACC / 60.0;
+                    new_rot_vel = new_rot_vel.min(0.0);
+                }
+
+                cannon.rot_vel = new_rot_vel;
+                cannon.facing = cannon.facing.rotate_by(cannon.rot_vel / 60.0);
+            },
+        };
         // apply an accelerating force in some direction
         // if let Some(rd) = self.input_state.cannon_rotate
         // {
@@ -85,14 +123,32 @@ impl crate::FixedUpdate<Cannon> for crate::MainState
         //     delta_rot = Cannon::ROT_DE_ACC * if cannon.rot_vel > 0.0 { -1.0 } else { 1.0 } * context.time.delta().as_secs_f32();
         // }
 
-        let mut new_rot_vel = cannon.rot_vel + delta_rot;
-        new_rot_vel = new_rot_vel.min(Cannon::MAX_ROT_PER_SEC);
-        cannon.rot_vel = new_rot_vel;
+        // let mut new_rot_vel = cannon.rot_vel + delta_rot;
+
+        // // println!("delta_rot: {delta_rot}, rot_vel: {}, facing: {:?}", cannon.rot_vel, cannon.facing);
+
+        // // new_rot_vel = new_rot_vel.min(Cannon::MAX_ROT_PER_SEC);
+        // new_rot_vel = new_rot_vel.clamp(-Cannon::MAX_ROT_PER_SEC, Cannon::MAX_ROT_PER_SEC);
+        // cannon.rot_vel = new_rot_vel;
+        
+        // println!(
+        //     "rot_vel: {}, rotation_per_frame: {}, facing angle: {}",
+        //     cannon.rot_vel,
+        //     cannon.rot_vel / 60.0,
+        //     cannon.facing.angle_between(Vec2::X)
+        // );
 
         // println!("{new_rot_vel}");
 
         // let new_facing = cannon.facing + delta_rot;
-        cannon.facing = cannon.facing.rotate_by(cannon.rot_vel / 60.0);
+        // cannon.facing = cannon.facing.rotate_by(cannon.rot_vel / 60.0);
+        // let rotation_per_frame = cannon.rot_vel / 60.0;
+        // let cos_theta = rotation_per_frame.cos();
+        // let sin_theta = rotation_per_frame.sin();
+        // cannon.facing = Vec2::new(
+        //     cannon.facing.x * cos_theta - cannon.facing.y * sin_theta,
+        //     cannon.facing.x * sin_theta + cannon.facing.y * cos_theta
+        // );
 
         // cannon.facing = new_facing;
         // cannon.facing = cannon.facing.rotate_by(cannon.rot_vel * context.time.delta().as_secs_f32());
