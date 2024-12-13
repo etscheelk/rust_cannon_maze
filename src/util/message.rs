4f4d8e583d9aef4bfce4147673e21570abe754ef
Lollo <41307169+etscheelk@users.noreply.github.com>
2024-12-12 23:17:00 -0600

    more message and cannon

--- src/util/message.rs
+++ src/util/message.rs
@@ -3,10 +3,10 @@ use serde::{Deserialize, Serialize};
 type Duration = f32;
 
 #[non_exhaustive]
-#[derive(Debug, Clone, Copy, Default, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub enum Message<I = ()>
 {
-    ActiveWithValue(I),
+    Active(I),
     ActiveTicking(I, Duration),
     
     #[default]
@@ -14,15 +14,22 @@ pub enum Message<I = ()>
 }
 
 impl<I> Message<I>
+where
+    I: std::fmt::Debug
 {
     pub fn create_active(value: I) -> Self
     {
-        Message::ActiveWithValue(value)
+        Message::Active(value)
     }
 
     pub fn create_active_ticking(value: I, duration: Duration) -> Self
     {
-        Self::ActiveTicking(value, duration)
+        Message::ActiveTicking(value, duration)
+    }
+
+    pub fn create_inactive() -> Self
+    {
+        Message::Inactive
     }
 
     pub fn /*(I am here haha)*/set_active_ticking(&mut self, value: I, duration: Duration)
@@ -30,13 +37,14 @@ impl<I> Message<I>
         *self = Message::ActiveTicking(value, duration);
     }
 
+
     /// Gets the associated value contained in the message, if it exists.
     pub fn is_active(&self) -> Option<&I>
     {
         use Message::*;
         match self
         {
-            ActiveWithValue(i) | ActiveTicking(i, _) =>
+            Active(i) | ActiveTicking(i, _) =>
             {
                 Some(i)
             },
@@ -46,13 +54,15 @@ impl<I> Message<I>
 
     pub fn tick(&mut self, dt: f32)
     {
+        println!("message: {:?}", *self);
+
         use Message::*;
 
         if let ActiveTicking(_, time) = self
         {
             *time -= dt;
 
-            if *time < 0.0
+            if *time <= 0.0
             {
                 *self = Inactive;
             }
